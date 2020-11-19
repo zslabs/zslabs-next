3eef3b3d0252bda63e88cb61a5cc46a5bec532c3
Zach Schnackel <info@zslabs.com>
2020-11-18 23:44:50 -0500

    more testing

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { MenuAlt2Outline } from 'heroicons-react'
 import Head from 'next/head'
 
 export default function Home() {
@@ -6,7 +7,21 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
       </Head>
-      hello form home
+      <div className="p-4">hello</div>
+      <div className="grid place-items-center gap-4">
+        <div className="uppercase text-gray-500 font-extrabold tracking-widest md:text-lg">
+          Full-stack/motion developer
+        </div>
+        <h1 className="text-6xl md:text-7xl font-extrabold tracking-tight">
+          Zach Schnackel
+        </h1>
+        <button
+          type="button"
+          className="rounded-full h-12 w-12 bg-black text-white ring-4 ring-gray-200 ring-offset-gray-500 transition duration-100 ease-in-out transform hover:scale-105"
+        >
+          <MenuAlt2Outline className="mx-auto text-gray-100" />
+        </button>
+      </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ module.exports = {
       fontFamily: {
         sans: ['ZS Sans', ...defaultTheme.fontFamily.sans],
       },
+      gridTemplateColumns: {
+        ...defaultTheme.gridTemplateColumns,
+        13: 'repeat(13, minmax(0, 1fr))',
+        14: 'repeat(14, minmax(0, 1fr))',
+        15: 'repeat(15, minmax(0, 1fr))',
+        16: 'repeat(16, minmax(0, 1fr))',
+      },
     },
+    purge: [
+      './components/**/*.{js,ts,jsx,tsx}',
+      './layouts/**/*.{js,ts,jsx,tsx}',
+      './media/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}',
+    ],
   },
 }
