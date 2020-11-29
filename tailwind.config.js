41175cdcb9998613aabdc37296d1b2b981ee66f4
Zach Schnackel <info@zslabs.com>
2020-11-29 09:34:35 -0500

    adjust container

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
         14: 'repeat(14, minmax(0, 1fr))',
         15: 'repeat(15, minmax(0, 1fr))',
         16: 'repeat(16, minmax(0, 1fr))',
+        '1/2': '0.5fr',
         '3/4': '0.75fr',
       },
       transitionProperty: {
