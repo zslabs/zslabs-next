8b8d248f41a36e77c2a39c9b80f8be6f9a0d95d3
Zach Schnackel <info@zslabs.com>
2020-12-14 23:06:02 -0500

    remove dark prose styles (apply is messing up, need to investigate) add in no-prose class

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -3,58 +3,28 @@ import renderToString from 'next-mdx-remote/render-to-string'
 import hydrate from 'next-mdx-remote/hydrate'
 
 import { getAllPosts, getPostBySlug } from '~lib/api'
+import AutoLinkHeader from '~components/AutoLinkHeader'
 
-const Test = () => <div>test custom component</div>
-const components = { Test }
+const Test = () => (
+  <div>
+    test custom <h2 className="relative no-prose">Something</h2>
+  </div>
+)
+const components = {
+  Test,
+  h1: (props) => <AutoLinkHeader as="h1" {...props} />,
+  h2: (props) => <AutoLinkHeader as="h2" {...props} />,
+  h3: (props) => <AutoLinkHeader as="h3" {...props} />,
+  h4: (props) => <AutoLinkHeader as="h4" {...props} />,
+  h5: (props) => <AutoLinkHeader as="h5" {...props} />,
+}
 
 export default function Post({ post }) {
   console.log(post)
 
   const content = hydrate(post.source, { components })
 
-  return (
-    <div className="prose">
-      {content}
-
-      <h2>Hello!!</h2>
-      <h3>Hello</h3>
-      <h4>Hello</h4>
-      <h5>Hello</h5>
-      <p>
-        Lorem ipsum dolor sit <a href="#">hello</a> amet consectetur adipisicing
-        elit. Quaerat, veniam unde velit accusantium quasi voluptas ipsam facere
-        illum maxime? Libero illo deleniti, odit quos nisi porro numquam rem aut{' '}
-        <strong>quaerat</strong>!
-      </p>
-      <ul>
-        <li>List item 1</li>
-        <li>
-          List item 2 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Et possimus vitae saepe expedita quam amet officia, delectus
-          consequatur? Ipsum ea quisquam maiores officiis a, ut tempore? Alias
-          reprehenderit eius culpa?
-        </li>
-        <li>List item 3</li>
-      </ul>
-      <ol>
-        <li>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
-          possimus doloremque facere natus necessitatibus voluptatem et enim
-          ullam nisi, dolores cumque quisquam dignissimos quasi eveniet quia?
-          Magnam accusamus assumenda recusandae.
-        </li>
-        <li>List item 2</li>
-        <li>List item 3</li>
-      </ol>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi magnam
-        assumenda, et id exercitationem autem, nostrum consequuntur distinctio
-        ea molestiae rerum. <code>hello</code> Iste numquam natus architecto, in
-        pariatur nihil sint hic.
-      </p>
-      <pre>this is something</pre>
-    </div>
-  )
+  return <div className="prose">{content}</div>
 }
 
 Post.propTypes = {
