f9a323377f44efcff446fbb0458b3445539381ed
Zach Schnackel <info@zslabs.com>
2022-01-10 20:20:05 -0500

    move back to npm

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Run type-check on changes to TypeScript files
-  '**/*.ts?(x)': () => 'yarn type:check',
+  '**/*.ts?(x)': () => 'npm run type:check',
   // Run ESLint on changes to JavaScript/TypeScript files
-  '**/*.(ts|js)?(x)': (filenames) => `yarn lint ${filenames.join(' ')}`,
+  '**/*.(ts|js)?(x)': (filenames) => `npm run lint ${filenames.join(' ')}`,
 }
