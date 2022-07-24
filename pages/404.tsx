477c0a1da7d5668b1bffa7f4822e41f3dce83875
Zach Schnackel <info@zslabs.com>
2022-07-24 16:29:07 -0400

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 
 import Container from '~components/Container'
+import Section from '~components/Section'
 import SEO from '~components/SEO'
 
 const PageNotFound: NextPage = () => {
@@ -8,12 +9,14 @@ const PageNotFound: NextPage = () => {
     <>
       <SEO title="Page not found" />
       <Container>
-        <div className="text-center">
-          <h1 className="mb-8 bg-gradient-to-br from-accent-9 to-primary-9 bg-clip-text text-9xl font-extrabold text-transparent">
-            404
-          </h1>
-          <p className="text-lg">It's probably something you did</p>
-        </div>
+        <Section>
+          <div className="text-center">
+            <h1 className="mb-8 bg-gradient-to-br from-accent-9 to-primary-9 bg-clip-text text-9xl font-extrabold text-transparent">
+              404
+            </h1>
+            <p className="text-lg">It's probably something you did</p>
+          </div>
+        </Section>
       </Container>
     </>
   )
